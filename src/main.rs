761d9483657f4931924cdfd6ea4bebeae094f01f
Dylan McKay <me@dylanmckay.io>
2017-08-21 20:37:49 +1200

    Get everything compiling

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,23 @@
-fn main() {
+#![feature(lang_items, unwind_attributes)]
+
+#![no_std]
+#![no_main]
+
+#[no_mangle]
+pub extern fn main() {
 }
+
+// These do not need to be in a module, but we group them here for clarity.
+pub mod std {
+    #[lang = "eh_personality"]
+    #[no_mangle]
+    pub unsafe extern "C" fn rust_eh_personality(state: (), exception_object: *mut (), context: *mut ()) -> () {
+    }
+
+    #[lang = "panic_fmt"]
+    #[unwind]
+    pub extern fn rust_begin_panic(msg: (), file: &'static str, line: u32) -> ! {
+        loop { }
+    }
+}
+
