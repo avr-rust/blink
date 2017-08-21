fda4c930edcf428b62b94fa4962dd8aec3065c9d
Dylan McKay <me@dylanmckay.io>
2017-08-21 20:50:57 +1200

    Add a small example program

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,44 @@
 #![no_std]
 #![no_main]
 
+extern crate arduino;
+
+use arduino::{DDRB, PORTB};
+use core::ptr::write_volatile;
+
 #[no_mangle]
 pub extern fn main() {
+    // Set all PORTB pins up as outputs
+    unsafe { write_volatile(DDRB, 0xFF) }
+
+    loop {
+        // Set all pins on PORTB to high.
+        unsafe { write_volatile(PORTB, 0xFF) }
+
+        small_delay();
+
+        // Set all pins on PORTB to low.
+        unsafe { write_volatile(PORTB, 0x00) }
+
+        small_delay();
+    }
+}
+
+/// A small busy loop.
+fn small_delay() {
+    for _ in 0..10000 { }
 }
 
 // These do not need to be in a module, but we group them here for clarity.
 pub mod std {
     #[lang = "eh_personality"]
     #[no_mangle]
-    pub unsafe extern "C" fn rust_eh_personality(state: (), exception_object: *mut (), context: *mut ()) -> () {
+    pub unsafe extern "C" fn rust_eh_personality(_state: (), _exception_object: *mut (), _context: *mut ()) -> () {
     }
 
     #[lang = "panic_fmt"]
     #[unwind]
-    pub extern fn rust_begin_panic(msg: (), file: &'static str, line: u32) -> ! {
+    pub extern fn rust_begin_panic(_msg: (), _file: &'static str, _line: u32) -> ! {
         loop { }
     }
 }
